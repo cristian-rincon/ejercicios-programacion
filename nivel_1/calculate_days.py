4626f42015c8b5c418a48c0e01d6d06b7004bf0d
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-06-23 14:18:46 -0500

    ejercicio 9 - Calculando dias

---
+++ nivel_1/calculate_days.py
@@ -0,0 +1,55 @@
+"""
+
+Reto #9 "Calculando días"
+==========================
+
+.. note::
+
+    Instrucciones: escribe un programa al que le indiques una cantidad de 
+    días y como resultado deberá mostrar cuantas horas, minutos y segundos 
+    hay en dicha cantidad de días.
+
+.. code-block:: python
+
+    if __name__ == "__main__":
+        
+        helper_text = print(""
+        Calculando días
+        ===============================================
+        Este programa te permitirá saber cuantas horas,
+        minutos y segundos hay en una cantidad de días 
+        dados.
+        "")
+
+        days = int(input(">>> Indica la cantidad de días a calcular: "))
+
+        minutes = (60 * 24) * days
+
+        seconds = 60 * minutes
+
+        print(f">>> En {days} días hay:")
+        print(f">>> {minutes} minutos.")
+        print(f">>> {seconds} segundos.")
+
+"""
+
+
+if __name__ == "__main__":
+
+    helper_text = print("""
+    Calculando días
+    ===============================================
+    Este programa te permitirá saber cuantas horas,
+    minutos y segundos hay en una cantidad de días 
+    dados.
+    """)
+
+    days = int(input(">>> Indica la cantidad de días a calcular: "))
+
+    minutes = (60 * 24) * days
+
+    seconds = 60 * minutes
+
+    print(f">>> En {days} días hay:")
+    print(f">>> {minutes} minutos.")
+    print(f">>> {seconds} segundos.")
