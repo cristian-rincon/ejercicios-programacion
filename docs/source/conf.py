6770802f46c3ab791a44c876aea6d50a5f5f6fb9
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-06-22 20:22:33 -0500

    ejercicio 5 - suma y multiplicacion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,9 @@ extensions = [
     "sphinx_rtd_theme",
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    "sphinx.ext.intersphinx",
+    'sphinx.ext.autosummary',
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,3 +59,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None), }
---
+++ nivel_1/sum_multiplication.py
@@ -0,0 +1,64 @@
+"""
+Reto #5 "Suma y multiplicación"
+================================
+
+.. note::
+
+    Instrucciones: añadiendo un extra al reto anterior 
+    ahora el usuario ingresará 3 números, sumarás los 2 
+    primeros y el resultado será multiplicado por el tercero. 
+    Añade las consideraciones del punto decimal del reto anterior.
+
+    Ejemplo:
+
+.. code-block:: python
+
+    Datos de entrada:2, 3, 4
+    Resultado:20
+
+"""
+
+
+def sum(a: int, b: int) -> int:
+    """Solicita dos números al usuario y los suma.
+
+    :type a: int
+    :param a: Primer número para sumar
+
+    :type b: int
+    :param b: Segundo número para sumar
+
+    :rtype: int
+    """
+
+    return a + b
+
+
+def multiplication(a: int, b: int) -> int:
+    """Solicita dos números al usuario y los multiplica.
+
+    :type a: int
+    :param a: Primer número para multiplicar
+
+    :type b: int
+    :param b: Segundo número para multiplicar
+
+    :rtype: int
+    """
+
+    return a * b
+
+
+if __name__ == "__main__":
+
+    helper_text_1 = print("""
+    Ingresa 3 números, los primeros dos se sumarán, 
+    el resultado se multiplicará por el tercero y 
+    el valor final se escribirá en pantalla.
+    """)
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
+
+    helper_text = print(f'El resultado es:')
+    print(multiplication(sum(num1,num2),num3))
