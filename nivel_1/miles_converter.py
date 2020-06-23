d687c20ff5964ff32822a7e2a17a6f5e96147de0
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-06-23 14:34:42 -0500

    ejercicio 10 - calculando millas

---
+++ nivel_1/miles_converter.py
@@ -0,0 +1,38 @@
+"""
+Reto #10 “Conversor de millas”
+===============================
+
+Instrucciones: hay 1.609344 km en una milla (mi). 
+Escribe un programa en el que el usuario indique 
+una cantidad de millas y muestre en pantalla el 
+resultado convertido a kilómetros.
+
+"""
+
+
+def miles_converter(miles):
+    """Calcula la equivalencia de millas en kilómetros.
+
+    :type miles: float
+    :param miles: Número de millas a convertir.
+
+    :rtype: float
+    """
+
+    km = 1.609344
+
+    result = km * miles
+
+    return result
+
+if __name__ == "__main__":
+    
+    print("Convertidor de millas a kilómetros")
+    print("==================================")
+    miles = float(input(">>> Ingresa la cantidad de millas a convertir: "))
+
+    result = miles_converter(miles)
+
+    print(f">>> {miles} millas equivalen a {result} kilómetros.")
+
+    
